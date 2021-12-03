33aaf484fa575f8c2d3599cfcac1e281bb10febd
Muneeb <thinkmuneeb@gmail.com>
2021-12-04 00:36:04 +0500

    links

--- src/Pages/MoreDetail.jsx
+++ src/Pages/MoreDetail.jsx
@@ -1,5 +1,5 @@
-import React from "react";
-import { MoreDetailMain, Bottom, DetailsTop } from "../Components";
+import React from 'react';
+import { MoreDetailMain, Bottom, DetailsTop } from '../Components';
 
 export default function Creators() {
   return (
@@ -11,17 +11,32 @@ export default function Creators() {
   );
 }
 /*
-Collections
-Creators
-Detail
-MoreDetail
-Detailed
-Wallet
-Testimonials
-Signin
-Signup
-Aboutus
-Buy
-Bid
-Sell
+https://wizardly-lichterman-432ef3.netlify.app/Collections
+https://wizardly-lichterman-432ef3.netlify.app/Creators
+https://wizardly-lichterman-432ef3.netlify.app/Detail
+https://wizardly-lichterman-432ef3.netlify.app/MoreDetail
+https://wizardly-lichterman-432ef3.netlify.app/Detailed
+https://wizardly-lichterman-432ef3.netlify.app/Wallet
+https://wizardly-lichterman-432ef3.netlify.app/Testimonials
+https://wizardly-lichterman-432ef3.netlify.app/Signin
+https://wizardly-lichterman-432ef3.netlify.app/Signup
+https://wizardly-lichterman-432ef3.netlify.app/Aboutus
+https://wizardly-lichterman-432ef3.netlify.app/Buy
+https://wizardly-lichterman-432ef3.netlify.app/Bid
+https://wizardly-lichterman-432ef3.netlify.app/Sell
+
+
+http://localhost:3000/Collections
+http://localhost:3000/Creators
+http://localhost:3000/Detail
+http://localhost:3000/MoreDetail
+http://localhost:3000/Detailed
+http://localhost:3000/Wallet
+http://localhost:3000/Testimonials
+http://localhost:3000/Signin
+http://localhost:3000/Signup
+http://localhost:3000/Aboutus
+http://localhost:3000/Buy
+http://localhost:3000/Bid
+http://localhost:3000/Sell
 */
